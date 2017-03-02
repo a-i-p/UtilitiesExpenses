f63a5486919fe65e9317d582a9bcca090488f968
a-i-p <anton.parfenov@gmail.com>
2017-03-02 16:52:10 +0300

    fix #6. Обновление списка тарифов и установка селекшена только в режиме дизайна.

--- UtilitiesExpenses/ViewModel/MainViewModel.cs
+++ UtilitiesExpenses/ViewModel/MainViewModel.cs
@@ -35,8 +35,15 @@ namespace UtilitiesExpenses.ViewModel
                   new NavigationService())
         {
 #if DEBUG
-            refresh();
-            SelectedTariff = TariffList[2];
+            if (System.Windows.Application.Current is App)
+            {
+                return;
+            }
+            else
+            {
+                refresh();
+                SelectedTariff = TariffList[2];
+            }
 #endif
         }
 
